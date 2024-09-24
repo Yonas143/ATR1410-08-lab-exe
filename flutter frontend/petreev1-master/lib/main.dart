e064dc8dce064c5e5ce015c0c82de2ad36e90d8b
Yonas143 <yoniwin.yw@gmail.com>
2024-09-24 08:49:25 +0000

    final commit

---
+++ flutter frontend/petreev1-master/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:ui_13/page/home_page.dart';
+
+import 'page/splash_page.dart';
+import 'page/splash_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Petree',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Gilroy',
+      ),
+      home: const SafeArea(
+        child: HomePage(),
+      ),
+    );
+  }
+}
